a5d33a338dfe9376359aebd0647c3c1e7f0371c7
lixiang <852213399@qq.com>
2018-12-28 00:28:29 +0800

    blank final

--- javacode-demo/src/main/java/indi/xianglee94/javacode/keywords/FinalDemo.java
+++ javacode-demo/src/main/java/indi/xianglee94/javacode/keywords/FinalDemo.java
@@ -5,17 +5,35 @@ public class FinalDemo {
         testFinal1();
     }
 
-    static void testFinal1(){
+    static void testFinal1() {
         String a = "helloworld";
-        String aa = "hello"+"world";
+        String aa = "hello" + "world";
         final String b = "hello";
         String c = "hello";
 
-        String d = b+"world";
-        String e = c+"world";
+        String d = b + "world";
+        String e = c + "world";
 
-        System.out.println(a==aa);//false
-        System.out.println(a==d);//false
-        System.out.println(a==e);//false
+        System.out.println(a == aa);//false
+        System.out.println(a == d);//false
+        System.out.println(a == e);//false
     }
+}
+
+class BlankFinal {
+    //blank final 必须在每个构造器里对blank final 进行赋值
+    private final int i = 0;
+    private final int j;
+    private final String s;
+
+    private BlankFinal(){
+        this.j = 1;
+        this.s = "";
+    }
+
+    private BlankFinal(int a){
+        j = a * 1;
+        s = "";
+    }
+
 }
