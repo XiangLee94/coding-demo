486e93c36fdc5bc88afcda31d7974c0b4c3b7c2a
lixiang <852213399@qq.com>
2019-11-10 12:42:15 +0800

    remove useless code

--- spring-demo/src/main/java/indi/xianglee94/springdemo/Test.java
+++ spring-demo/src/main/java/indi/xianglee94/springdemo/Test.java
@@ -1,4 +1,7 @@
 package indi.xianglee94.springdemo;
 
 public class Test {
+    public static void main(String[] args) {
+
+    }
 }
--- spring-demo/src/main/java/indi/xianglee94/springdemo/config/JavaConfig.java
+++ spring-demo/src/main/java/indi/xianglee94/springdemo/config/JavaConfig.java
@@ -1,4 +1,6 @@
 package indi.xianglee94.springdemo.config;
 
+import javax.inject.Qualifier;
+
 public class JavaConfig {
 }
